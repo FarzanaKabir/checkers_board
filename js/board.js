6cab1ffc80f0f544d038b2fb207464335bcd4885
farzanakabir <farzanakabirrinky05@gmail.com>
2021-01-17 18:02:42 +0600

    recode for move forward and change color

--- js/board.js
+++ js/board.js
@@ -1,84 +1,166 @@
-$(document).ready(function () {
-    $('#board-container').html(renderBoard())
-    renderCheckers()
-})
-
-function renderBoard() {
-    return `
-        ${renderRow(1)}
-        ${renderRow(2)}
-        ${renderRow(3)}
-        ${renderRow(4)}
-        ${renderRow(5)}
-        ${renderRow(6)}
-        ${renderRow(7)}
-        ${renderRow(8)}
-    `
+var board = new Array(8);
+for (var i = 0; i < board.length; i++) {
+    board[i] = new Array(8);
+}
+var figureColor = {
+    white: '<span class="white icon-smiley"></span>',
+    black: '<span class="black icon-smiley"></span>'
 }
 
-function renderRow(rowNum) {
-    return `
-        <div id="row-${rowNum}" class="row">
-            ${renderCell(rowNum, 1)}
-            ${renderCell(rowNum, 2)}
-            ${renderCell(rowNum, 3)}
-            ${renderCell(rowNum, 4)}
-            ${renderCell(rowNum, 5)}
-            ${renderCell(rowNum, 6)}
-            ${renderCell(rowNum, 7)}
-            ${renderCell(rowNum, 8)}
-        </div>
-    `
-}
-
-function renderCell(rowNum, cellNum) {
-    if (cellColor(cellNum, rowNum) === 'white') {
-        return `<div id="cell-${rowNum}-${cellNum}" class="cell white"></div>`
-    } else
-        return `<div id="cell-${rowNum}-${cellNum}" class="cell black"></div>`
-}
-
-/*** Helper Methods ***/
-function parity(num) {
-    return (num % 2 == 1) ? 'even' : 'odd'
-}
-
-function cellColor(cellNum, rowNum) {
-    return parity(cellNum) == parity(rowNum) ? 'white' : 'black'
-}
-
-function moveSelectedCheckerHere() {
-    console.log('things')
-    if(selectedChecker) {
-        console.log(`move checker here`)
-        let whiteCell = $(this)
-        console.log(`white cell: `, whiteCell)
-        let id = whiteCell.attr('id')
-        console.log(`id: `, id)
-        let idParts = id.split('-')
-        console.log(`idParts = `, idParts)
-
-        selectedChecker.row = Number(idParts[1])
-        selectedChecker.cell = Number(idParts[2])
-
-        console.log(`the checker I'm moving is `, selectedChecker.color)
-        if (selectedChecker.color == `white` && selectedChecker.row == 1) {
-            console.log(`I'm moving a black checker to the white home row`)
-            selectedChecker.isKing = true
-        } else if (selectedChecker.color == `black` && selectedChecker.row == 8) {
-            console.log(`I'm moving a red checker to the black home row`)
-            selectedChecker.isKing = true
+function initBoard() {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            if (i == board.length/2 || i == board.length/2-1) {
+                break;
+            }
+            if (!((i+j)%2)) {
+                if (i < board.length/2) {
+                    board[i][j] = figureColor.white;
+                }
+                else {
+                    board[i][j] = figureColor.black;
+                }
+            }
         }
+    }
+}
+initBoard();
+document.write('<table><tr><td onclick="myFunction(this)">')
+for (var i = 0; i < board.length; i++) {
+    board[i] = board[i].join('</td><td onclick="myFunction(this)">');
+}
+document.write(board.join('</td></tr><tr><td onclick="myFunction(this)">'));
+document.write('</td></tr></table>');
 
-        selectedChecker = undefined
-        renderCheckers()
-    } else {
-        console.log(`select a checker, foo!`)
+
+var table = document.getElementsByTagName('table')[0];
+cells = table.getElementsByTagName('td');
+var switcher = true;
+
+var wPlayer = document.getElementById('wPlayerScore');
+var bPlayer = document.getElementById('bPlayerScore');
+var wPlayerScore = 0;
+var bPlayerScore = 0;
+
+
+function myFunction(x) {
+    if ($(x).hasClass('move') ) {
+        move(x);
+    }
+    else if ($(x).hasClass('step') && !($(x).children().length)) {
+        step(x);
+    }
+    if (!($(x).hasClass('selected')) && !($(x).hasClass('step'))) {
+        var cellIndex = x.cellIndex;
+        var rowIndex = x.parentNode.rowIndex;
+        var $thisIndex = cellIndex+rowIndex*8;
+        var $this = $(cells[$thisIndex]);
+        var $black = $this.children().hasClass('black');
+        var $white = $this.children().hasClass('white');
+        if ($this.children().length) {
+            $(cells).removeClass('selected step move moved');
+            if ($black && !switcher) {
+                $this.addClass('selected');
+                //left step
+                if (!($(cells[$thisIndex-9]).children().length) && ($thisIndex)%8) {
+                    var $stepIndex1 = $thisIndex-9;
+                }
+                if ($(cells[$thisIndex-9]).children().hasClass('white') && !($(cells[$thisIndex-18]).children().length) && ($thisIndex-1)%8 && $thisIndex > 15) {
+                    $(cells[$thisIndex-18]).addClass('step move');
+                    $(cells[$thisIndex-9]).addClass('moved');
+                }
+                //left back step
+                if ($(cells[$thisIndex+9]).children().hasClass('white') && !($(cells[$thisIndex+18]).children().length) && ($thisIndex+2)%8 && $thisIndex < 48) {
+                    $(cells[$thisIndex+18]).addClass('step move');
+                    $(cells[$thisIndex+9]).addClass('moved');
+                }
+
+                //right step
+                if (!($(cells[$thisIndex-7]).children().length) && ($thisIndex+1)%8) {
+                    var $stepIndex2 = $thisIndex-7;
+                }
+                if ($(cells[$thisIndex-7]).children().hasClass('white') && !($(cells[$thisIndex-14]).children().length) && ($thisIndex+2)%8 && $thisIndex > 15) {
+                    $(cells[$thisIndex-14]).addClass('step move');
+                    $(cells[$thisIndex-7]).addClass('moved');
+                }
+                //right back step
+                if ($(cells[$thisIndex+7]).children().hasClass('white') && !($(cells[$thisIndex+14]).children().length) && ($thisIndex-1)%8 && $thisIndex < 48) {
+                    $(cells[$thisIndex+14]).addClass('step move');
+                    $(cells[$thisIndex+7]).addClass('moved');
+                }
+            }
+            else if ($white && switcher) {
+                $this.addClass('selected');
+                //left step
+                if (!($(cells[$thisIndex+9]).children().length) && ($thisIndex+1)%8) {
+                    var $stepIndex1 = $thisIndex+9;
+                }
+                if ($(cells[$thisIndex+9]).children().hasClass('black') && !($(cells[$thisIndex+18]).children().length) && ($thisIndex+2)%8 && $thisIndex < 48) {
+                    $(cells[$thisIndex+18]).addClass('step move');
+                    $(cells[$thisIndex+9]).addClass('moved');
+                }
+                //left back step
+                if ($(cells[$thisIndex-9]).children().hasClass('black') && !($(cells[$thisIndex-18]).children().length) && ($thisIndex-1)%8 && $thisIndex > 15) {
+                    $(cells[$thisIndex-18]).addClass('step move');
+                    $(cells[$thisIndex-9]).addClass('moved');
+                }
+                //right step
+                if (!($(cells[$thisIndex+7]).children().length) && ($thisIndex)%8) {
+                    var $stepIndex2 = $thisIndex+7;
+                }
+                if ($(cells[$thisIndex+7]).children().hasClass('black') && !($(cells[$thisIndex+14]).children().length) && ($thisIndex-1)%8 && $thisIndex < 48) {
+                    $(cells[$thisIndex+14]).addClass('step move');
+                    $(cells[$thisIndex+7]).addClass('moved');
+                }
+                //right back step
+                if ($(cells[$thisIndex-7]).children().hasClass('black') && !($(cells[$thisIndex-14]).children().length) && ($thisIndex+2)%8 && $thisIndex > 15) {
+                    $(cells[$thisIndex-14]).addClass('step move');
+                    $(cells[$thisIndex-7]).addClass('moved');
+                }
+            }
+            //monster
+            if (($black && $thisIndex < 8) || ($white && $thisIndex > 55)) {
+                $this.children().addClass('monster');
+                if (($black && $thisIndex < 8)) {
+                    bPlayerScore += 3;
+                }
+                else {
+                    wPlayerScore += 3;
+                }
+                score();
+            }
+            $(cells[$stepIndex1]).toggleClass('step');
+            $(cells[$stepIndex2]).toggleClass('step');
+        }
+        else {
+            $(cells).removeClass('selected step move moved');
+        }
     }
 }
 
-function clearBoard() {
-    $(`.white.cell`).html(``)
-    $(`.white.cell`).unbind('click')
-    $(`.out-of-play`).html(``)
+
+function move(x) {
+    $(cells).removeClass('step');
+    if ($('.moved').children().hasClass('white')) {
+        bPlayerScore++;
+    }
+    else if ($('.moved').children().hasClass('black')) {
+        wPlayerScore++;
+    }
+    score();
+    $('.moved')[0].innerHTML = '';
+    $(cells).removeClass('moved move');
+    step(x);
 }
+function step(x) {
+    x.innerHTML = $('.selected')[0].innerHTML;
+    $('.selected')[0].innerText = '';
+    $(cells).removeClass('selected step moved move');
+    switcher = !switcher;
+}
+
+function score() {
+    wPlayer.innerHTML = wPlayerScore;
+    bPlayer.innerHTML = bPlayerScore;
+}
+
